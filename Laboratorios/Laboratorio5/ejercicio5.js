08ab8166b7aaa50bc6338c1d7eb35fae85ffba6e
marielosagui <00078913@uca.edu.sv>
2019-09-23 11:03:25 -0600

    ejercicio

--- Laboratorios/Laboratorio5/ejercicio5.js
+++ Laboratorios/Laboratorio5/ejercicio5.js
@@ -3,6 +3,38 @@ de derecha a izquierda y viceversa.
 Crear una función en JavaScript que reciba como parámetro una palabra. 
 La función debe retornar si la palabra es palíndroma o no.  */
 
-function palíndroma(palabra){
-var palabra
+function palindromo(cadena){
+    var resultado="La cadena \"" + cadena+"\"\n";
+    var cadenaOrignal=cadena.toLowerCase();
+    var letraEspacios= cadenaOrignal.split("");
+
+    var cadenaSinespacios="";
+    for (i in letraEspacios){
+        if(letraEspacios[i] !=""){
+            cadenaSinespacios+= letraEspacios[i];
+        }
+    }
+    var cadenaSinespacios="";
+    for(i in letraEspacios){
+        if (letraEspacios[i] !=""){
+            cadenaSinespacios+=letraEspacios[i];
+        }
+    }
+    var letras=cadenaSinespacios.split("");
+    var letrasalreves=cadenaSinespacios.split("").reverse();
+
+    var iguales=true;
+    for (i in letras){
+        if(letras[i]==letrasalreves[i]){
+    }else{
+        iguales=false;
+    }
+}
+if(iguales){
+    resultado+= "es palindromo";
+}
+else{
+    resultado+="no es palindromo";
+}
+return resultado;
 }
