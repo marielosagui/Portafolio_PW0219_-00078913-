1fb49df137462c7a8abfa97ff6251c126ce083c5
marielosagui <00078913@uca.edu.sv>
2019-09-23 10:54:53 -0600

    ejercicio8

--- Laboratorios/Laboratorio5/ejercicio8.js
+++ Laboratorios/Laboratorio5/ejercicio8.js
@@ -3,4 +3,22 @@ La función debe devolver el valor en binario.  */
 
 function binario (numero){
 
+    var array=[];
+    var entrada=0;
+    var resultado='';
+
+    if(n==0){
+        return n;
+    }
+    while (n>0){
+        residuo=parseInt(n%2);
+        n=parseInt(n/2);
+        array[entrada]=residuo;
+        entrada++
+    }
+for(var i=array.length-1;i>=0;i--){
+    resultado+=array[i];
+}
+
+return resultado;
 }
