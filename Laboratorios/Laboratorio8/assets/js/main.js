0e5e39cb7ac986f725f33e0b70a6ab79dc6f9ba5
marielosagui <00078913@uca.edu.sv>
2019-10-16 20:59:47 -0600

    js modified, carnet confirmation

--- Laboratorios/Laboratorio8/assets/js/main.js
+++ Laboratorios/Laboratorio8/assets/js/main.js
@@ -1,103 +1,79 @@
-var all_rows = []
-var id_counter = 0;
+var rows =[];
+var counterId = 0;
 
 var parseLateSwitch = (value) => {
     if(value){
-        return "Tarde :("
+        return "Tarde :(";
     }
-    return "A tiempo :)"
+    return "A tiempo :)";
 }
-
-function addRow(carnet, schedule, late, tbody){
+function addRow(carnet,schedule,late,tbody){
     var newRow = document.createElement("tr");
     var date = new Date();
-
-   
-    all_rows.push({
-        "id" : id_counter,
+    rows.push({
+        "id" :counterId,
         "carnet": carnet,
         "schedule": schedule,
-        "late": late
+        "late" : late
     })
 
-    newRow.innerHTML =
-    `    <td><b>${carnet}</b></td>
-        <td>${schedule}</td>
-        <td>${date.toLocaleString()}</td>
-        <td>${late}</td>`
-
-    // nueva celda que conedrá el boton
-    let cell_conteiner = document.createElement("td")
-
-    let new_btn = document.createElement("button")
-    new_btn.classList.add("btn")
-    new_btn.classList.add("btn-danger")
-    new_btn.classList.add("mx-2")
-
-    new_btn.innerText = "Drop"
-   
-    new_btn.value  = id_counter
+    console.log(rows);
+    newRow.innerHTML = 
+            `<td><b>${carnet}</b></td>
+            <td>${schedule}</td>
+            <td>${date.toLocaleString()}</td>
+            <td>${late}</td> `;
 
 
-    new_btn.addEventListener("click", (event)=>{
-        let id_element = event.srcElement.value
+    var cellContainer = document.createElement("td");
+    var deleteButton = document.createElement("button");
 
-       
-        let element_node = document.querySelector(`tr>td>button[value='${id_element}']`).parentElement.parentElement
-        table_body.removeChild(element_node)
 
-        all_rows.forEach((item, index) =>{
-            if(item.id == id_element){
-                all_rows.splice(index, 1)
+    var cellContainer2 = document.createElement("td");
+    var validInput = document.createElement("input");
+    validInput.id="input"+counterId;
+    validInput.type = Text;
+
+    
+    deleteButton.classList.add("btn");
+    deleteButton.classList.add("btn-danger");
+    deleteButton.innerText ="Eliminar";
+    deleteButton.value = counterId;
+
+    deleteButton.addEventListener("click",function(event){
+
+        var idElement = event.srcElement.value;
+        //para eliminar
+        var trToDelete = document.querySelector(`button[value='${idElement}']`).parentElement.parentElement;
+
+        //para validar el carnet y luego eliminar
+        var validCarnet = document.querySelector(`#input${idElement}`).value;
+
+
+        rows.forEach((item,index) => {
+            if(item.id == idElement){ //validacion
+                if(item.carnet == validCarnet)
+                {
+                    tbody.removeChild(trToDelete)
+                    rows.splice(index, 1);
+                    //si el carnet no coincide con el ingresado mensaje alerta
+                }else{
+                    alert("Carnet no coincide")
+                }
             }
         })
     })
 
-     let new_btn_2 = document.createElement("button")
-    new_btn_2.classList.add("btn")
-    new_btn_2.classList.add("btn-primary")
-    new_btn_2.classList.add("mx-2") 
+    cellContainer.appendChild(deleteButton);
+    newRow.appendChild(cellContainer);
 
-   // new_btn_2.innerHTML = "<i class='fas fa-edit font-2'>"
-    new_btn_2.value  = id_counter
-
-    new_btn_2.addEventListener("click", (event)=>{
-        var id_element = new_btn_2.value
-        console.log(id_element);
-        all_rows.forEach((item, index) =>{
-            if(item.id == id_element){
-              fillForm(item);
-            }
-        })
-    })
-
-    cell_conteiner.appendChild(new_btn)
-    cell_conteiner.appendChild(new_btn_2)
-    newRow.appendChild(cell_conteiner)
-
+    cellContainer2.appendChild(validInput);
+    newRow.appendChild(cellContainer2);
     tbody.appendChild(newRow);
-
-    id_counter++
-};
-
-function fillForm(item)
-{
-  var carnet_field = document.querySelector("#carnet_field");
-  var schedule_field = document.querySelector("#schedule_field");
-  var late_switch = document.querySelector("#late_switch");
-
-  carnet_field.value = item.carnet;
-  schedule_field.value = item.schedule;
-
-  if(item.late == "Tarde :("){
-    late_switch.checked = true;
-  }
-  else{
-    late_switch.checked = false;
-  }
+    counterId++;
 }
 
-window.onload = function()
+window.onload = function() 
 {
     var submit_btn = document.querySelector("#submit_btn");
     var carnet_field = document.querySelector("#carnet_field");
@@ -107,23 +83,26 @@ window.onload = function()
 
     var carnetRegex = new RegExp('[0-9]{8}');
 
-    submit_btn.addEventListener("click", function(){
-        var carnet = carnet_field.value;
+    submit_btn.addEventListener("click",()=>{
+        var carnet =carnet_field.value;
         var schedule = schedule_field.options[schedule_field.selectedIndex].text;
         var late = parseLateSwitch(late_switch.checked);
+        //debe cumplir con el formato de 8 digitos
         if(carnetRegex.test(carnet)){
-            addRow(carnet, schedule, late, tBody);
+            addRow(carnet,schedule,late,tBody);
         }else{
-            alert("Formato no valido")
+            alert("Formato no valido");
         }
     });
 
-    carnet_field.addEventListener("keyup", (event)=>{
-        var carnet = carnet_field.value;
+
+    carnet_field.addEventListener("keyup",(event) =>{
+        var carnet =carnet_field.value;
+        //para boton, se habilita solo si cumple con formato
         if(carnetRegex.test(carnet)){
             submit_btn.disabled = false;
         }else{
             submit_btn.disabled = true;
         }
     });
-}
+};
